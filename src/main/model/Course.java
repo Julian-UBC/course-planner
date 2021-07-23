a69105c6525de3e649b03790939a85e5897a450b
Julian <julian.widjaja02@gmail.com>
2021-07-23 16:58:08 -0700

    Finish implementing Course.java

--- src/main/model/Course.java
+++ src/main/model/Course.java
@@ -9,67 +9,76 @@ public class Course {
     // Status: planning, ongoing, completed
     private String status;
 
+    private static final String PLANNING = "planning";
+    private static final String ONGOING = "ongoing";
+    private static final String COMPLETED = "completed";
+
     // REQUIRES: name not an empty string, credit > 0
     // EFFECTS: initializes a course with given name and credit, planning as status, and grade = 0
     public Course(String name, int credit) {
-        //stub
+        courseName = name;
+        courseCredit = credit;
+        status = PLANNING;
+        grade = 0;
     }
 
     // REQUIRES: name not an empty string
     // MODIFIES: this
     // EFFECTS: change the course's name to the given name
     public void changeCourseName(String name) {
-        //stub
+        courseName = name;
     }
 
     // REQUIRES: credit > 0
     // MODIFIES: this
     // EFFECTS: change the course's credit to the given credit
     public void changeCourseCredit(int credit) {
-        //stub
+        courseCredit = credit;
     }
 
     // MODIFIES: this
     // EFFECTS: set status to ongoing
     public void setOngoing() {
-        //stub
+        status = ONGOING;
     }
 
     // MODIFIES: this
     // EFFECTS: set status to completed
     public void setCompleted() {
-        //stub
+        status = COMPLETED;
     }
 
     // MODIFIES: this
     // EFFECTS: set status to planning
     public void setPlanning() {
-        //stub
+        status = PLANNING;
     }
 
     // MODIFIES: this
     // EFFECTS: set my course's grade to the given grade & set the course's status to completed
     public int setGrade(int grade) {
-        return 0; //stub
+        this.grade = grade;
+        status = COMPLETED;
+        return this.grade;
     }
 
     // EFFECTS: return the course's name
     public String getCourseName() {
-        return ""; //stub
+        return courseName;
     }
 
     // EFFECTS: return the course's credit
     public int getCourseCredit() {
-        return 0; //stub
+        return courseCredit;
     }
 
     // EFFECTS: return the status of the course
     public String getCourseStatus() {
-        return ""; //stub
+        return status;
     }
 
     // EFFECTS: return the grade gotten in the course
     public int getCourseGrade() {
-        return 0; //stub
+        return grade;
     }
 }
